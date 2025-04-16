5c3d8b3925024dda71803827417fee0c0e46cf10
Casey Benzing <benzing.c@northeastern.edu>
2025-04-16 17:37:45 -0400

    initial for a6

--- src/Kambaz/Account/Navigation.tsx
+++ src/Kambaz/Account/Navigation.tsx
@@ -1,7 +1,9 @@
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 export default function AccountNavigation() {
   const { currentUser } = useSelector((state: any) => state.accountReducer);
+  const { pathname } = useLocation();
+  const active = (path: string) => (pathname.includes(path) ? "active" : "");
   const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
   return (
     <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
@@ -9,5 +11,7 @@ export default function AccountNavigation() {
         <Link to={`/Kambaz/Account/${link}`} id={`wd-course-${link}-link`}
       className="list-group-item active border border-0"> {link} </Link>
       ))}
+      {currentUser && currentUser.role === "ADMIN" && (
+        <Link to={`/Kambaz/Account/Users`} className={`list-group-item ${active("Users")}`}> Users </Link> )}
   </div>
 );}
